d6f6b72fb2af0fbe682fe8c29c7a8e60c52bad14
DuebassLei <1130122701@qq.com>
2019-10-31 10:55:32 +0800

    :apple: add deploy.sh

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -36,19 +36,13 @@ module.exports = {
             '/study/other/': getOtherSidebar("Other Sheet","More"),
             '/favorite/': ['']
         },
+        base: "/vuepress-blog/",
         // 假定 GitHub。也可以是一个完整的 GitLab URL。
         repo: 'https://github.com/DuebassLei/vuepress-blog',
         // 自定义项目git remote add origin https://github.com/DuebassLei/vuepress-blog.git仓库链接文字
         // 默认根据 `themeConfig.repo` 中的 URL 来自动匹配是 "GitHub"/"GitLab"/"Bitbucket" 中的哪个，如果不设置时是 "Source"。
         repoLabel: '贡献代码！',
         // 以下为可选的 "Edit this page" 链接选项
-        // 如果你的文档和项目位于不同仓库：
-        docsRepo: 'https://github.com/DuebassLei/vuepress-blog',
-        // 如果你的文档不在仓库的根目录下：
-        docsDir: 'docs',
-        // 如果你的文档在某个特定的分支（默认是 'master' 分支）：
-        docsBranch: 'master',
-        // 默认为 false，设置为 true 来启用
         editLinks: true,
         // 自定义编辑链接的文本。默认是 "Edit this page"
         editLinkText: '帮助我们改进页面内容！'
